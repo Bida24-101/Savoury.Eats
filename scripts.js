265b70809d4df32f82e2c719618168b4c73adc31
Bida24-101 <bida24-101@thuto.bac.ac.bw>
2025-03-31 02:35:17 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 
+// Counter for liked recipes
+let likeCount = 0;
+
+// Function to handle liking a recipe
+function likeRecipe(recipeName) {
+    alert(`You liked the recipe: ${recipeName}`);
+    likeCount++;
+    document.getElementById('like-count-text').textContent = likeCount;
+}
+
+// Toggle Dark/Light theme
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+}
